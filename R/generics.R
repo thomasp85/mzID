a19d1bab519b2639051eca2bf165ffac5420564d
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2013-04-25 16:48:41 +0200

    Added flatten generic and documentation

---
+++ R/generics.R
@@ -0,0 +1,25 @@
+#' Flatten an mzID related class into a table
+#' 
+#' This function flattens the content of the object into a table by merging the content intelligently (it knows the links
+#' between the different objects).
+#' 
+#' @param object The object to be flattened
+#' 
+#' @param ... Additional parameters (currently unused)
+#' 
+#' @return A \code{data.frame} with the flattened result
+#' 
+#' @seealso \code{\link{mzID-class}}
+#' 
+#' @export
+#' @docType methods
+#' @rdname flatten-methods
+#' 
+#' @examples
+#' mzResults <- mzID("http://psi-pi.googlecode.com/svn/trunk/examples/1_1examples/55merge_tandem.mzid")
+#' head(flatten(mzResults))
+#' 
+setGeneric(
+  'flatten', 
+  def=function(object, ...){standardGeneric('flatten')}
+  )
