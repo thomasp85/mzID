52927d1018ae52715c668a441fa2e9e35ce3fa57
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2013-07-24 10:02:13 +0200

    Added missing parameter to flatten method

--- R/mzID.R
+++ R/mzID.R
@@ -119,7 +119,7 @@ setMethod(
 #' 
 setMethod(
     'flatten', 'mzID',
-    function(object) {
+    function(object, no.redundancy=TRUE) {
        flatPSM <- flatten(object@psm)
        flatPSM <- flatPSM[, colnames(flatPSM) != 'id']
        flatEviData <- 
